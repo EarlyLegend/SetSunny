f7d084182149aadb205ffe72a95e6a314600483d
Cedric Denoo <abc_denoo@hotmail.com>
2012-02-11 13:52:40 +0100

    Signed-off-by: Cedric Denoo <abc_denoo@hotmail.com>

--- src/me/cedi/setsunny/setsunny.java
+++ src/me/cedi/setsunny/setsunny.java
@@ -3,46 +3,48 @@ package me.cedi.setsunny;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class setsunny extends JavaPlugin{
-	@Override
 	public void onDisable() {
 		System.out.println("SetSunny v" + getDescription().getVersion() + " Disabled!");
 	}
-
-	@Override
 	public void onEnable() {
 		System.out.println("SetSunny v" + getDescription().getVersion() + " Enabled!");
 	}
-	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[]args){
-		
-		Player p = (Player)sender;
-		World w = p.getWorld();
-		
-		if(cmd.getName().equalsIgnoreCase("setsunny") && (sender.hasPermission("SetWeather.sunny") || sender.isOp())){
-			w.setThundering(false);
-			w.setStorm(false);
-			w.setWeatherDuration(1000000);
-			p.sendMessage("Weather set to sunny.");
+			
+			if(sender instanceof Player){
+				World w = ((Player) sender).getWorld();
+				if(cmd.getName().equalsIgnoreCase("setsunny") && (sender.hasPermission("SetWeather.sunny") || sender.isOp())){
+					w.setThundering(false);
+					w.setStorm(false);
+					w.setWeatherDuration(1000000);
+					sender.sendMessage("Weather set to sunny.");
+					return true;
+				}
+				if(cmd.getName().equalsIgnoreCase("setstormy") && (sender.hasPermission("SetWeather.stormy") || sender.isOp())){
+					w.setThundering(true);
+					w.setStorm(true);
+					w.setWeatherDuration(1000000);
+					sender.sendMessage("Weather set to stormy.");
+					return true;
+				}
+				if(cmd.getName().equalsIgnoreCase("setrainy") && (sender.hasPermission("SetWeather.rainy") || sender.isOp())){
+					w.setThundering(false);
+					w.setStorm(true);
+					w.setWeatherDuration(1000000);
+					sender.sendMessage("Weather set to rainy.");
+					return true;
+				}
+				return false;
+			}
+			else if(sender instanceof ConsoleCommandSender){
+				System.out.println("Cannot run this from console!");
+				return true;
+			}
 			return true;
-		}
-		if(cmd.getName().equalsIgnoreCase("setstormy") && (sender.hasPermission("SetWeather.stormy") || sender.isOp())){
-			w.setThundering(true);
-			w.setStorm(true);
-			w.setWeatherDuration(1000000);
-			p.sendMessage("Weather set to stormy.");
-			return true;
-		}
-		if(cmd.getName().equalsIgnoreCase("setrainy") && (sender.hasPermission("SetWeather.rainy") || sender.isOp())){
-			w.setThundering(false);
-			w.setStorm(true);
-			w.setWeatherDuration(1000000);
-			p.sendMessage("Weather set to rainy.");
-			return true;
-		}
-		return false;
 	}
 }
