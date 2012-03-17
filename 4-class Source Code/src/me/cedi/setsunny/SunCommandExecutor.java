9382d82ff3b5f20f2c8772a212bc0c195930edfa
EarlyLegend <earlylegend@hotmail.co.uk>
2012-03-17 19:22:55 +0000

    Update 4-class Source Code/src/me/cedi/setsunny/SunCommandExecutor.java

--- 4-class Source Code/src/me/cedi/setsunny/SunCommandExecutor.java
+++ 4-class Source Code/src/me/cedi/setsunny/SunCommandExecutor.java
@@ -7,6 +7,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 
 public class SunCommandExecutor implements CommandExecutor{
 	SetSunnyCore plugin;
@@ -15,7 +16,7 @@ public class SunCommandExecutor implements CommandExecutor{
 		plugin = instance;
 	}
 	
-	@Override
+	@EventHandler
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 //onCommand(start)-------------------------------------------------		
 		if (sender instanceof ConsoleCommandSender){
