982fa040503f182e3b3e0ad86ec3b1f8a9e408aa
EarlyLegend <earlylegend@hotmail.co.uk>
2012-03-17 19:18:57 +0000

    Update 4-class Source Code/src/me/cedi/setsunny/RainCommandExecutor.java

--- 4-class Source Code/src/me/cedi/setsunny/RainCommandExecutor.java
+++ 4-class Source Code/src/me/cedi/setsunny/RainCommandExecutor.java
@@ -7,7 +7,9 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 
+@EventHandler
 public class RainCommandExecutor implements CommandExecutor{
 	
 	SetSunnyCore plugin;
